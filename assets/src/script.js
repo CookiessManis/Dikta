deec7bc237373436497fc810e1e7a7bb59cfab48
Rayhan Renaldy Hafiza Purnama <96982909+CookiessManis@users.noreply.github.com>
2023-11-23 15:47:08 +0700

    23/11/2023

--- assets/src/script.js
+++ assets/src/script.js
@@ -15,18 +15,20 @@ const vets = document.querySelector("#vets");
 const settings = document.querySelector("#settings");
 const text1 = document.querySelector(".text1");
 const text2 = document.querySelector(".text2");
+const text3 = document.querySelector(".text3");
+const text4 = document.querySelector(".text4");
+const pengumuman = document.querySelector(".pengumuman-container");
+
 const containerDashboard = document.querySelector("#container-content");
 const logo_satpolpp = document.querySelector("#logo-satpolpp");
+const dokumen = document.getElementById("document");
+const register = document.getElementById("register");
 
 let previousToggled = null;
 let currentToggled = null;
 
 search.addEventListener("click", (e) => {
   toggleMenu(search);
-});
-
-dashboard.addEventListener("click", (e) => {
-  toggleMenu(dashboard);
 });
 
 const toggleMenu = (button) => {
@@ -62,7 +64,8 @@ const openMenu = () => {
   toggleMenu(menu);
 
   text1.style.display = "none";
-  text2.style.display = "none";
+  text3.style.display = "none";
+  text4.style.display = "none";
 
   // logo satpol pp
   // logo_satpolpp.style.paddingLeft = "220px";
@@ -79,12 +82,26 @@ const openMenu = () => {
   search.style.justifyContent = "left";
   search.appendChild(p_search);
 
-  let p_dash = document.createElement("p");
-  p_dash.id = "p-dashboard";
-  p_dash.innerHTML = "Pegawai";
-  dashboard.style.width = "220px";
-  dashboard.style.justifyContent = "left";
-  dashboard.appendChild(p_dash);
+  // let p_dash = document.createElement("p");
+  // p_dash.id = "p-dashboard";
+  // p_dash.innerHTML = "Pegawai";
+  // dashboard.style.width = "220px";
+  // dashboard.style.justifyContent = "left";
+  // dashboard.appendChild(p_dash);
+
+  let p_dokumen = document.createElement("p");
+  p_dokumen.id = "p-document";
+  p_dokumen.innerHTML = "Document";
+  dokumen.style.width = "220px";
+  dokumen.style.justifyContent = "left";
+  dokumen.appendChild(p_dokumen);
+
+  let p_register = document.createElement("p");
+  p_register.id = "p-register";
+  p_register.innerHTML = "Register";
+  register.style.width = "220px";
+  register.style.justifyContent = "left";
+  register.appendChild(p_register);
 
   let user_container = document.createElement("div");
   user_container.id = "user-container";
@@ -96,6 +113,9 @@ const openMenu = () => {
   let user_role = document.createElement("p");
   user_role.id = "user-role";
   user_role.innerHTML = "Veterinarian";
+
+  // dashboard pengumuman
+  // pengumuman.style.paddingLeft = "220px";
 
   user_container.appendChild(user_name);
   user_container.appendChild(user_role);
@@ -113,15 +133,27 @@ const closeMenu = () => {
   logo_satpolpp.style.justifyContent = "center";
 
   text1.style.display = "block";
-  text2.style.display = "block";
+  text3.style.display = "block";
+  text4.style.display = "block";
 
   search.removeChild(document.getElementById("p-search"));
   search.style.width = "80px";
   search.style.justifyContent = "center";
 
-  dashboard.removeChild(document.getElementById("p-dashboard"));
-  dashboard.style.width = "80px";
-  dashboard.style.justifyContent = "center";
+  dokumen.removeChild(document.getElementById("p-document"));
+  dokumen.style.width = "80px";
+  dokumen.style.justifyContent = "center";
+
+  register.removeChild(document.getElementById("p-register"));
+  register.style.width = "80px";
+  register.style.justifyContent = "center";
+
+  // dashboard.removeChild(document.getElementById("p-dashboard"));
+  // dashboard.style.width = "80px";
+  // dashboard.style.justifyContent = "center";
+
+  // dashboard
+  // pengumuman.style.paddingLeft = "92px";
 
   sidebar.classList.remove("active");
   sidebar.style.width = "120px";
